bdb6b6446a655375cfe3295f06cbb80bd53b43c0
Dell Du <18588220928@163.com>
2020-09-05 17:32:27 +0800

    Update.

--- noise_level.py
+++ noise_level.py
@@ -2,71 +2,59 @@ import torch
 from torchvision import transforms
 from PIL import Image
 
-# import pdb
+import pdb
 
 
-def TensorNoiseLevel(t, PatchSize=32, use_GPU=False):
+def TensorNoiseLevel(t, PatchSize=8):
 	'''
-		input: t -- 2-dimensions tensor
+		input: t -- Image CxHxW tensor, [0, 1.0]
 	'''
+	step = 3
+	C, H, W = t.size()
+	PatchNumbers = len(range(0, H - PatchSize, step)) * len(range(0, W - PatchSize, step))
+	PlainPatchSize = C * PatchSize * PatchSize
+
+	# Collect patchs
+	patchs = torch.zeros(PlainPatchSize, PatchNumbers)
 	nn = 0
-	matrix = torch.zeros(PatchSize, PatchSize)
-
-	if use_GPU:
-		t = t.cuda()
-		matrix = matrix.cuda()
-
-	step = max(PatchSize//2, 1)
-	for i in range(0, t.size(0) - PatchSize, step):
-		for j in range(0, t.size(1) - PatchSize, step):
-			patch = t[i : i + PatchSize, j : j + PatchSize]
-			patch = patch - patch.mean()
-			matrix = matrix + patch.mul(patch.t())
+	for i in range(0, H - PatchSize, step):
+		for j in range(0, W - PatchSize, step):
+			patch = t[:, i : i + PatchSize, j : j + PatchSize]
+			patchs[:, nn] = patch.reshape(PlainPatchSize)
 			nn = nn + 1
-	matrix = matrix.div(nn)
 
-	eigvals, _ = torch.eig(matrix)
+	m = patchs.mean(dim = 1, keepdims=True)
+	patchs = patchs - m
+
+	matrix = torch.matmul(patchs, patchs.t())/PatchNumbers
 
 	# [Real, Image] Eigen Values
-	sigmas, _ = eigvals[0 : PatchSize * PatchSize, 0].sort()
+	eigvals, _  = torch.eig(matrix)
+	sigmas, _ = eigvals[0 : PlainPatchSize, 0].sort()
 
-	# Calibration ...
-	output = sigmas.clamp(0.0, 100.0)
-	output = output.sqrt()
-	output = output * 255.0
-	x = output[PatchSize//4].item()
-	index = PatchSize//2
-	if x < 5.0:
-		index = PatchSize//32
-	if x < 10.0:
-		index = PatchSize//16
-	if x < 20.0:
-		index = PatchSize//8
-	if x < 40.0:
-		index = PatchSize//4
-	if x < 80.0:
-		index = PatchSize//2
-	# x >= 80.0, ==> index == PatchSize // 2
-	x = output[index].item()
-	y = x - 1.0
-	if x < 40.0:
-		y = y - 1.0
-	if x < 20.0:
-		y = y - 2.0
-	if x < 10.0:
-		y = y - 3.0
+	# sigmas --- Real of egien values
+	del patchs, matrix
 
-	return max(y, 0.0)
+	# Search noise level
+	for i in range(PlainPatchSize, 0, -1):
+		x = sigmas[:i]
+		m = x.mean()
+		left = (x < m).sum()
+		right = (x > m).sum()
+		if left == right:
+			return m.sqrt()
 
+	# Search failure ?
+	return sigmas.median().sqrt()
 
 def ImageNoiseLevel(image_filename):
-	img = Image.open(image_filename).convert('L')
+	img = Image.open(image_filename).convert('RGB')
 	t = transforms.ToTensor()(img)
-	t = t.squeeze(0)
-	for i in range(0, 90, 5):
+
+	for i in [5, 15, 10, 25, 30, 40, 50, 60, 70, 80, 90, 100]:
 		std = i / 255.0
 		noise = t + std * torch.randn(t.size())
-		sigma = TensorNoiseLevel(noise)
+		sigma = TensorNoiseLevel(noise) * 255.0
 
 		# if abs(i - sigma) > 1.0:
 		print("{} Noise Level: Real is {:.4f}, Estimate is {:.4f}".format(image_filename, i, sigma))
@@ -89,3 +77,6 @@ if __name__ == '__main__':
 	ImageNoiseLevel("test/output/09_noise.png")
 
 	# ImageNoiseLevel("/tmp/bag.png")
+
+	# ImageNoiseLevel("/tmp/test.jpg")	
+	# ImageNoiseLevel("/tmp/lena.png")
